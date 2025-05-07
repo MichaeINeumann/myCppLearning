3fed2047e5a14a05c4121d8f2cf65788f8d81c88
Michael Neumann <michael.neumann@m-a-i.de>
2025-05-07 09:11:30 +0200

    Add tests for subtraction, multiplication and division

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -18,6 +18,34 @@ TEST_CASE("Addition negativ nummbers funktioniert", "[rechner]") {
     REQUIRE(result.value() == Catch::Approx(-34.0));
 }
 
+TEST_CASE("Subtraktion funktioniert", "[rechner]") {
+    Rechner r(10.0, 5.0, '-');
+    auto result = r.berechne();
+    REQUIRE(result.has_value());
+    REQUIRE(result.value() == Catch::Approx(5.0));
+}
+
+TEST_CASE("Multiplikation funktioniert", "[rechner]") {
+    Rechner r(3.0, 4.0, '*');
+    auto result = r.berechne();
+    REQUIRE(result.has_value());
+    REQUIRE(result.value() == Catch::Approx(12.0));
+}
+
+TEST_CASE("Multiplikation mit Null funktioniert", "[rechner]") {
+    Rechner r(0.0, 4.0, '*');
+    auto result = r.berechne();
+    REQUIRE(result.has_value());
+    REQUIRE(result.value() == Catch::Approx(0.0));
+}
+
+TEST_CASE("Division funktioniert", "[rechner]") {
+    Rechner r(10.0, 2.0, '/');
+    auto result = r.berechne();
+    REQUIRE(result.has_value());
+    REQUIRE(result.value() == Catch::Approx(5.0));
+}
+
 TEST_CASE("Division durch Null liefert kein Ergebnis", "[rechner]") {
     Rechner r(10.0, 0.0, '/');
     auto result = r.berechne();
